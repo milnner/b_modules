f73dea15d33647f77eef0017548ca94cd91437da
milnner <milnnernj@gmail.com>
2023-10-05 16:58:08 -0300

    adicionando enviroment

---
+++ environment/env.go
@@ -0,0 +1,70 @@
+package environment
+
+import (
+	"os"
+
+	errapp "github.com/milnner/b_modules/errors"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var Environment *environment
+
+type environment struct {
+	jwtSecretKey *string
+	logPath      *string
+	certFile     string
+	keyFile      string
+	addr         string
+	debug        bool
+}
+
+func (u *environment) InitEnvironment(logPathEnvVariable, jwtSecretKeyEnvVariable string) {
+	u.logPath = new(string)
+	*(u.logPath) = os.Getenv(logPathEnvVariable)
+
+	if *(u.logPath) == "" {
+		panic(errapp.NewNotExistEnvironmentVariableError(jwtSecretKeyEnvVariable))
+	} else if _, err := os.Stat(*(u.logPath)); os.IsNotExist(err) {
+		panic(err)
+	}
+
+	u.jwtSecretKey = new(string)
+	*(u.jwtSecretKey) = os.Getenv(jwtSecretKeyEnvVariable)
+
+	if *(u.jwtSecretKey) == "" {
+		panic(errapp.NewNotExistEnvironmentVariableError(jwtSecretKeyEnvVariable))
+	}
+
+	if u.certFile == "" {
+		panic("Certificate file is required for HTTPS")
+	}
+
+	if u.keyFile == "" {
+		panic("Key file is required for HTTPS")
+	}
+}
+
+func (u *environment) GetAddr() string {
+	return u.addr
+}
+
+func (u *environment) GetJWTSecretKey() []byte {
+	return []byte(*(u.jwtSecretKey))
+}
+
+func (u *environment) GetLogPath() string {
+	return *(u.logPath)
+}
+
+func (u *environment) GetCertFile() string {
+	return u.certFile
+}
+
+func (u *environment) GetKeyFile() string {
+	return u.keyFile
+}
+
+func (u *environment) IsDebug() bool {
+	return u.debug
+}
