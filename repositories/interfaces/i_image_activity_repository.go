24da8663e3b5bb176f03a0b88ab75c47e0441784
milnner <milnnernj@gmail.com>
2023-12-26 10:01:17 -0300

    adcionando interfaces para repositorios

---
+++ repositories/interfaces/i_image_activity_repository.go
@@ -0,0 +1,12 @@
+package interfaces
+
+import "github.com/milnner/b_modules/models"
+
+type IImageActivityRepository interface {
+	Insert(*models.ImageActivity) error
+	Update(*models.ImageActivity) error
+	Delete(*models.ImageActivity) error
+	GetImageActivityById(*models.ImageActivity) error
+	GetImageActivitiesByIds([]models.ImageActivity) error
+	GetImageActivitiesByAreaId(*models.Area) ([]models.ImageActivity, error)
+}
---
+++ repositories/interfaces/i_text_activity_repository.go
@@ -0,0 +1,14 @@
+package interfaces
+
+import (
+	"github.com/milnner/b_modules/models"
+)
+
+type ITextActivityRepository interface {
+	Insert(*models.TextActivity) error
+	Update(*models.TextActivity) error
+	Delete(*models.TextActivity) error
+	GetTextActivityById(*models.TextActivity) error
+	GetTextActivitiesByIds([]models.TextActivity) error
+	GetTextActivitiesByAreaId(*models.Area) ([]models.TextActivity, error)
+}
