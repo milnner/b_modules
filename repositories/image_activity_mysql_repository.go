ba9947bf6711cb4987d770b78c199631126cbd45
milnner <milnnernj@gmail.com>
2023-12-26 10:01:40 -0300

    adicionando repositorios

---
+++ repositories/image_activity_mysql_repository.go
@@ -0,0 +1,110 @@
+package repositories
+
+import (
+	"database/sql"
+	"encoding/hex"
+	"fmt"
+	"time"
+
+	errapp "github.com/milnner/b_modules/errors"
+	"github.com/milnner/b_modules/models"
+)
+
+type ImageActivityMySQLRepository struct {
+	db *sql.DB
+}
+
+func NewImageActivityMySQLRepository(db *sql.DB) (*ImageActivityMySQLRepository, error) {
+	if db == nil {
+		return nil, errapp.NewDatabaseConnectionError()
+	}
+	return &ImageActivityMySQLRepository{db: db}, nil
+}
+
+func (u *ImageActivityMySQLRepository) Insert(imageActivity *models.ImageActivity) (err error) {
+	query := "INSERT INTO `image_activities`(`area_id`, `title`, `_blob`) VALUES (?, ?, ?)"
+
+	if _, err = u.db.Exec(query, imageActivity.AreaId, imageActivity.Title, hex.EncodeToString(imageActivity.Blob)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *ImageActivityMySQLRepository) Delete(imageActivity *models.ImageActivity) (err error) {
+	query := "DELETE FROM `image_activities` WHERE `id`=?"
+	_, err = u.db.Exec(query, imageActivity.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *ImageActivityMySQLRepository) Update(imageActivity *models.ImageActivity) (err error) {
+	query := "UPDATE `image_activities` SET `title`=?,`_blob`=?,`last_update`=? WHERE `id`=?"
+	_, err = u.db.Exec(query, imageActivity.Title, imageActivity.Blob, imageActivity.LastUpdate.String()[:19], imageActivity.Id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *ImageActivityMySQLRepository) GetImageActivityById(imageActivity *models.ImageActivity) (err error) {
+	var (
+		rows       *sql.Rows
+		lastUpdate string
+	)
+	query := "SELECT `id`, `area_id`, `title`, `_blob`, `last_update`, `activated` FROM `image_activities` WHERE id=?"
+	rows, err = u.db.Query(query, imageActivity.Id)
+
+	if err != nil {
+		return err
+	}
+
+	if rows.Next() {
+		if err = rows.Scan(&imageActivity.Id, &imageActivity.AreaId, &imageActivity.Title, &imageActivity.Blob, &lastUpdate, &imageActivity.Activated); err != nil {
+			return err
+		}
+		if imageActivity.LastUpdate, err = time.Parse(time.DateTime, lastUpdate); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (u *ImageActivityMySQLRepository) GetImageActivitiesByIds(imageActivities []models.ImageActivity) (err error) {
+
+	for i := 0; i < len(imageActivities); i++ {
+		err = u.GetImageActivityById(&imageActivities[i])
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (u *ImageActivityMySQLRepository) GetImageActivitiesByAreaId(area *models.Area) (imageActivities []models.ImageActivity, err error) {
+	var (
+		imageActivity models.ImageActivity
+		rows          *sql.Rows
+		lastUpdate    string
+	)
+
+	query := "SELECT `id`, `area_id`, `title`, `_blob`, `last_update`, `activated` FROM `image_activities` WHERE area_id=?"
+	rows, err = u.db.Query(query, area.Id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		if err = rows.Scan(&imageActivity.Id, &imageActivity.AreaId, &imageActivity.Title, &imageActivity.Blob, &lastUpdate, &imageActivity.Activated); err != nil {
+			return nil, err
+		}
+		if imageActivity.LastUpdate, err = time.Parse(time.DateTime, lastUpdate); err != nil {
+			return nil, err
+		}
+		fmt.Println(imageActivities)
+		imageActivities = append(imageActivities, imageActivity)
+	}
+	return imageActivities, nil
+}
---
+++ repositories/text_activity_mysql_repository.go
@@ -0,0 +1,99 @@
+package repositories
+
+import (
+	"database/sql"
+	"encoding/hex"
+	"time"
+
+	"github.com/milnner/b_modules/models"
+)
+
+type TextActivityMySQLRepository struct {
+	db *sql.DB
+}
+
+func NewTextActivityMySQLRepository(db *sql.DB) *TextActivityMySQLRepository {
+	return &TextActivityMySQLRepository{db: db}
+}
+
+func (u *TextActivityMySQLRepository) Insert(textActivity *models.TextActivity) (err error) {
+	statement := "INSERT INTO `text_activities`(`area_id`, `title`, `_blob`) VALUES (?, ?, ?)"
+
+	if _, err = u.db.Exec(statement, textActivity.AreaId, textActivity.Title, hex.EncodeToString(textActivity.Blob)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *TextActivityMySQLRepository) Delete(textActivity *models.TextActivity) (err error) {
+	statement := "UPDATE `text_activities` SET `activated`=0 WHERE `id`=?"
+	if _, err = u.db.Exec(statement, textActivity.Id); err != nil {
+		return err
+	}
+	textActivity = nil
+	return nil
+}
+
+func (u *TextActivityMySQLRepository) Update(textActivity *models.TextActivity) (err error) {
+	statement := "UPDATE `text_activities` SET `title`=?,`_blob`=?,`last_update`=? WHERE `id`=?"
+	if _, err = u.db.Exec(statement, textActivity.Title, textActivity.Blob, textActivity.LastUpdate.String()[:19], textActivity.Id); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *TextActivityMySQLRepository) GetTextActivityById(textActivity *models.TextActivity) (err error) {
+	query := "SELECT `area_id`, `title`, `_blob`, `last_update`, `activated` FROM `text_activities` WHERE `id`=?"
+	var textActivityRow *sql.Rows
+	if textActivityRow, err = u.db.Query(query, textActivity.Id); err != nil {
+		return err
+	}
+
+	var lastUpdateStr string
+	if textActivityRow.Next() {
+		if err = textActivityRow.Scan(&textActivity.AreaId, &textActivity.Title, &textActivity.Blob, &lastUpdateStr, &textActivity.Activated); err != nil {
+			return err
+		}
+		var lastUpdate time.Time
+		if lastUpdate, err = time.Parse(time.DateTime, lastUpdateStr); err != nil {
+			return err
+		}
+		textActivity.LastUpdate = lastUpdate
+	}
+	return nil
+}
+
+func (u *TextActivityMySQLRepository) GetTextActivitiesByIds(textActivities []models.TextActivity) (err error) {
+	for i := 0; i < len(textActivities); i++ {
+		if err = u.GetTextActivityById(&textActivities[i]); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (u *TextActivityMySQLRepository) GetTextActivitiesByAreaId(area *models.Area) (textActivities []models.TextActivity, err error) {
+	var (
+		textActivityRow *sql.Rows
+		lastUpdateStr   string
+		textActivity    models.TextActivity
+	)
+	query := "SELECT `id`, `area_id`, `title`, `_blob`, `last_update`, `activated` FROM `text_activities` WHERE area_id=?"
+
+	if textActivityRow, err = u.db.Query(query, area.Id); err != nil {
+		return nil, err
+	}
+
+	for textActivityRow.Next() {
+		if err = textActivityRow.Scan(&textActivity.Id, &textActivity.AreaId, &textActivity.Title, &textActivity.Blob, &lastUpdateStr, &textActivity.Activated); err != nil {
+			return nil, err
+		}
+		var lastUpdate time.Time
+		if lastUpdate, err = time.Parse(time.DateTime, lastUpdateStr); err != nil {
+			return nil, err
+		}
+		textActivity.LastUpdate = lastUpdate
+		textActivities = append(textActivities, textActivity)
+	}
+	return textActivities, nil
+}
