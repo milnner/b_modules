66474ff771a68a0b0865d41529524b494dc9bf45
milnner <milnnernj@gmail.com>
2024-02-08 11:08:54 -0300

    datatype signup atualizado

--- apptypes/sign_up_user_type.go
+++ apptypes/sign_up_user_type.go
@@ -4,9 +4,9 @@ type SignUpUserType struct {
 	Name      string `json:"name"`
 	Surname   string `json:"surname"`
 	Email     string `json:"email"`
+	Professor uint8  `json:"professor"`
 	EntryDate string `json:"entry-date"`
 	BournDate string `json:"bourn-date"`
 	Sex       Sex    `json:"sex"`
 	Password  string `json:"password"`
-	PasswordR string `json:"password-r"`
 }
