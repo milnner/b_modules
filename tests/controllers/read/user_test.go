93120a12877dbdfdd18f004f7dbf35e23460d5df
milnner <milnnernj@gmail.com>
2024-08-22 11:05:06 -0300

    Fix read user to past

--- tests/controllers/read/user_test.go
+++ tests/controllers/read/user_test.go
@@ -9,114 +9,60 @@ import (
 	"testing"
 
 	readCtrl "github.com/milnner/b_modules/controllers/read"
+	"github.com/milnner/b_modules/database"
 	"github.com/milnner/b_modules/models"
 	"github.com/milnner/b_modules/repositories"
-	authSvc "github.com/milnner/b_modules/services/auth"
-	"github.com/milnner/b_modules/tokens"
-
-	"github.com/milnner/b_modules/database"
 	"github.com/milnner/b_modules/tests/config"
 )
 
-func TestCreateArea(t *testing.T) {
+func TestReadUser(t *testing.T) {
+	config.SetDBData()
 	config.SetRootDatabaseConn()
-	var err error
-	var dbConn *sql.DB
+	var (
+		err    error
+		dbConn *sql.DB
+	)
 
 	defer func() {
-		if err = database.
-			InitDatabaseConn(&dbConn,
-				config.
-					DatabaseConn.
-					User.GetDelete(),
-				"mysql"); err != nil {
+		if err = database.InitDatabaseConn(&dbConn, config.DatabaseConn.User.GetDelete(), "mysql"); err != nil {
 			t.Fatal(err)
 		}
-		if _, err = dbConn.
-			Exec("DELETE FROM area WHERE 1"); err != nil {
+		if _, err = dbConn.Exec("DELETE FROM users WHERE 1"); err != nil {
 			t.Fatal(err)
 		}
-		if _, err = dbConn.
-			Exec("DELETE FROM users WHERE 1"); err != nil {
-			t.Fatal(err)
-		}
-
 	}()
-
-	if err = database.
-		InitDatabaseConn(&dbConn,
-			config.DatabaseConn.
-				User.GetInsert(),
-			"mysql"); err != nil {
+	if err = database.InitDatabaseConn(&dbConn, config.DatabaseConn.User.GetInsert(), "mysql"); err != nil {
 		t.Fatal(err)
 	}
 	for i := 0; i < len(config.Users); i++ {
-		if _, err = dbConn.
-			Exec(config.Users[i]); err != nil {
+		if _, err = dbConn.Exec(config.Users[i]); err != nil {
 			t.Fatal(err)
 		}
 	}
-	for i := 0; i < len(config.Area); i++ {
-		if _, err = dbConn.
-			Exec(config.Area[i]); err != nil {
-			t.Fatal(err)
-		}
-	}
-
-	// Preparando o jwt
-	var token string
-	tkz := tokens.NewUserJWTokenizator(config.JwtSecretKey)
-	user := config.UsersObjs[0]
-
-	if err = authSvc.
-		NewAuthenticationSvc(&user,
-			&token,
-			tkz).
-		Run(); err != nil {
-		t.Fatal(err)
-	}
-
-	if err = database.
-		InitDatabaseConn(&dbConn,
-			config.DatabaseConn.
-				User.GetInsert(),
-			"mysql"); err != nil {
+	if err = database.InitDatabaseConn(&dbConn, config.DatabaseConn.User.GetSelect(), "mysql"); err != nil {
 		t.Fatal(err)
 		return
 	}
-	areaRepo, err := repositories.NewAreaMySQLRepository(dbConn)
+
+	userRepo, err := repositories.NewUserMySQLRepository(dbConn)
 	if err != nil {
 		t.Fatal(err)
 		return
 	}
+	ctrl := readCtrl.NewReadUserController(userRepo, config.Logger)
 
-	ctrl := readCtrl.NewReadAreaController(areaRepo, config.Logger, tkz, "mysql")
-
-	var buff []byte
-	body := bytes.NewBuffer(buff)
-	area := models.Area{OwnerId: 8, Id: config.AreasObjs[0].Id}
-
-	err = json.NewEncoder(body).Encode(area)
-
+	var b []byte
+	bd := bytes.NewBuffer(b)
+	user := models.User{Email: config.UsersObjs[0].Email}
+	err = json.NewEncoder(bd).Encode(user)
 	wr := httptest.NewRecorder()
-	req := httptest.NewRequest("POST", "/", body)
 
-	req.Header.Set("Authorization", "Bearer "+token)
+	req := httptest.NewRequest("GET", "/", bd)
 
 	ctrl.Handler(wr, req)
+
+	err = json.NewDecoder(wr.Body).Decode(&user)
 	if wr.Result().StatusCode != http.StatusOK {
-		t.Errorf("Not Ok, body  %v", wr.Body.String())
+		t.Errorf("Not Ok %v, status %v", user, wr.Result().StatusCode)
 	}
-
-	area = models.Area{OwnerId: 8, Id: config.AreasObjs[0].Id}
-	err = json.NewEncoder(body).Encode(area)
-
-	wr = httptest.NewRecorder()
-	req = httptest.NewRequest("POST", "/", body)
-	req.Header.Set("Authorization", "Bearer invalid_token")
-	ctrl.Handler(wr, req)
-	if wr.Result().StatusCode == http.StatusOK {
-		t.Errorf("Ok, but need to be Not Ok, body  %v", wr.Body.String())
-	}
-
 }
