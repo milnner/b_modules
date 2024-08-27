23040fa5e332378e65ff512ab233af608f4f4599
milnner <milnnernj@gmail.com>
2024-08-27 17:09:07 -0300

    Area permission fix

--- services/read/area_permission.go
+++ services/read/area_permission.go
@@ -3,6 +3,7 @@ package read
 import (
 	"log"
 
+	"github.com/milnner/b_modules/apptypes"
 	"github.com/milnner/b_modules/models"
 	iRepository "github.com/milnner/b_modules/repositories/interfaces"
 )
@@ -18,5 +19,16 @@ func NewReadAreaPermissionSvc(userArea *models.UserHasAreaAccess, repo iReposito
 }
 
 func (u *ReadAreaPermissionSvc) Run() error {
+	area := models.Area{Id: u.userArea.Area.Id, OwnerId: u.userArea.User.Id}
+	if err := u.repo.GetAreaById(&area); err != nil {
+		return err
+	}
+
+	if area.Activated != 0 &&
+		area.OwnerId == u.userArea.User.Id {
+		u.userArea.User.Permision = apptypes.Permission(apptypes.UserAreaPermissions.Write())
+		return nil
+	}
+
 	return u.repo.GetPermission(&u.userArea.Area, &u.userArea.User)
 }
