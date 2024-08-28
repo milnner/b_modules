2a985c7dc377cbbd5c6d2790fb54a9e7afcd958d
milnner <milnnernj@gmail.com>
2024-08-28 10:30:38 -0300

    Add equals method

--- models/class.go
+++ models/class.go
@@ -16,3 +16,14 @@ type Class struct {
 func NewClass(id int, title string, description string, creationDate time.Time, userCreatorId int, areaId int, lastUpdate time.Time, activated uint8) *Class {
 	return &Class{Id: id, Title: title, Description: description, CreationDate: creationDate, UserCreatorId: userCreatorId, AreaId: areaId, LastUpdate: lastUpdate, Activated: activated}
 }
+
+func (c Class) Equals(other Class) bool {
+	return c.Id == other.Id &&
+		c.Title == other.Title &&
+		c.Description == other.Description &&
+		c.CreationDate.Equal(other.CreationDate) &&
+		c.UserCreatorId == other.UserCreatorId &&
+		c.AreaId == other.AreaId &&
+		c.LastUpdate.Equal(other.LastUpdate) &&
+		c.Activated == other.Activated
+}
